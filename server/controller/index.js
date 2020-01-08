eb6adcc7a36d82a0b88ac043335782c5b3bd2b91
ProDaddy <undefended0@gmail.com>
2020-01-08 12:46:08 +0530

    Added error handling middleware

---
+++ server/controller/index.js
@@ -0,0 +1,11 @@
+module.exports.notFound = (req,res,next)=>{
+    const err = new Error('Not Found')
+    err.status= 404
+    next(err)
+}
+
+module.exports.error = (err,req,res,next)=>{
+    res.status(err.status || 400).json({
+        message:err.message || 'Something went wrong'
+    })
+}
--- server/index.js
+++ server/index.js
@@ -3,18 +3,24 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const control = require('./controller')
+
 const app = express()
 
 //middleware
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(bodyParser.json())
 
-app.get('/' , (req,res)=>{
-    res.json({
-        "hello": "world"
-    })
+app.get('/', (req, res,next) => {
+    next(Error('just a TEST'))
 })
 
 const port = process.env.PORT || 4000
+
+//Error Handling 
+app.use(control.notFound)
+app.use(control.error)
 
 app.listen(port, () => console.log(`Listing @${port}`))
