a783f738334b7391662c2f5898e959cc8414b4f4
ProDaddy <undefended0@gmail.com>
2020-01-26 18:43:08 +0530

    Some temporary styling ;p

---
+++ client/src/components/Logo/Logo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import myLogo from '../../assets/images/logo1.png'
+
+export default ()=> <div style={{height:'100%',padding:'8px',boxSizing:'border-box'}}>
+        <img src={myLogo} alt="my_logo" style={{height:'100%',width:'40%'}}/>
+    </div>
--- client/src/components/ToolBar/Toolbar.jsx
+++ client/src/components/ToolBar/Toolbar.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
+
+import Logo from '../../components/Logo/Logo'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
+    justifyContent:'space-between',
+    flex: 1,
+    height:'56px',
+    width:'100%',
+    top:0,
+    left:0,
+    position:'sticky',
+    zIndex:90,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -17,6 +24,11 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  toolbar:{
+    height:'100%',
+    flex:1,
+    justifyContent:'space-between'
+  }
 }));
 
 export default function ToolBar() {
@@ -24,11 +36,10 @@ export default function ToolBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            Brand
-          </Typography>
+      <AppBar position="static" style={{height:'100%'}}>
+        <Toolbar className={classes.toolbar}>
+          <Logo/>
+          <Button color="inherit">Register</Button>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
