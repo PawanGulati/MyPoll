e7aff6a3b7f10f918255bda1155aa644f2ed2c3a
ProDaddy <undefended0@gmail.com>
2020-02-05 19:55:23 +0530

    Temprarory logo and welcome page

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 import {Typography} from '@material-ui/core'
 
-import myLogo from '../../assets/images/logo1.png'
+import myLogo from '../../assets/images/logo.png'
 
-export default ()=> <div style={{height:'100%',padding:'8px',boxSizing:'border-box'}}>
-        <Typography variant='h4'>Brand</Typography>  
+export default ()=> <div style={{height:'100%',padding:'4px',boxSizing:'border-box',display:'flex'}}>
+        <img src={myLogo} style={{height:'93%',marginRight:'15px'}}/>
+        <div>
+            <Typography style={{fontFamily:'\'Black Ops One\',cursive',fontSize:'1rem',letterSpacing:'.1rem',lineHeight:'120%',}}><span style={{fontFamily:'\'Black Ops One\',cursive',fontSize:'1.5rem',letterSpacing:'.1rem'}}>M</span>Y</Typography>  
+            <Typography style={{fontFamily:'\'Black Ops One\',cursive',fontSize:'1rem',letterSpacing:'.1rem'}}><span style={{fontFamily:'\'Black Ops One\',cursive',fontSize:'1.5rem',letterSpacing:'.1rem'}}>P</span>OLLS</Typography>  
+        </div>
     </div>
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export default () =><div>
-            <h1 style={{color:'secondary'}}>Welcome</h1>    
-        </div>
-    
+import myLogo from '../assets/images/logo.png'
+
+import { Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles( theme =>({
+    logo:{
+        color:"secondary",
+        fontSize:'4rem',
+        fontFamily:'\'Black Ops One\',cursive'
+    },
+    span:{
+        fontFamily:'\'Black Ops One\',cursive',
+        fontSize:'6rem',
+        letterSpacing:'.1rem',
+        color:"secondary"
+    }
+}));
+
+export default () =>{
+        const classes = useStyles();
+        return(
+            <div>
+                <img src={myLogo}/>
+                <Typography className={classes.logo}><span className={classes.span}>M</span>Y POLL<span className={classes.span}>S</span></Typography>    
+                <Typography variant='h6'>Select respective POLL to contribute your vote</Typography>
+            </div>
+        )
+}
 
